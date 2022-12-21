8e9dc3216ea55d91c6deabc72929a33c4cb12430
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-12-21 16:55:33 +0700

    Create program menu

--- exercise5/Program.cs
+++ exercise5/Program.cs
@@ -62,6 +62,52 @@ namespace exercise5
         }
         static void Main(string[] args)
         {
+            Queue q = new Queue();
+            char ch;
+            while (true)
+            {
+                try
+                {
+
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("2. Implement delete operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice (1-4):");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                q.insert();
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.delete();
+                            }
+                            break;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid option!!");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered.");
+                }
+            }
         }
     }
 }
